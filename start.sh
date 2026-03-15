cf789a913c6dc14f2383bb04b290059b456c11dd
ryuto <ryuto.kiwako@gmail.com>
2026-03-15 23:44:16 +0900

    Add Render deployment files

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+php artisan config:clear
+php artisan cache:clear
+
+php artisan migrate --force
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+apache2-foreground
