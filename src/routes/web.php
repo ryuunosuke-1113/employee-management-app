c2f610f43d5aefaec6cb42d95b20b78c48aed18f
ryuto <ryuto.kiwako@gmail.com>
2026-04-07 21:00:15 +0900

    Fix root route

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
